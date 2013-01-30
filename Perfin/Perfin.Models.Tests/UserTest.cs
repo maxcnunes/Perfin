2b8705c2b4395049f5cde17f1d397b583eca0ad2
Max Claus Nunes <maxcnunes@gmail.com>
2013-01-30 01:24:41 -0200

    Adding test to user

--- Perfin/Perfin.Models.Tests/UserTest.cs
+++ Perfin/Perfin.Models.Tests/UserTest.cs
@@ -13,5 +13,30 @@ namespace Perfin.Models.Tests
             Assert.IsNotNull(user);
             Assert.IsInstanceOfType(user, typeof(User));
         }
+
+        [TestMethod]
+        public void Should_Set_Values_For_The_Properties()
+        {
+            var user = new User();
+
+            var id = 10;
+            var login = "login";
+            var email = "email";
+            var password = "password";
+            var name = "name"; 
+
+            user.Id = id;
+            user.Login = login;
+            user.Email = email;
+            user.Password = password;
+            user.Name = name;
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual(id, user.Id);
+            Assert.AreEqual(login, user.Login);
+            Assert.AreEqual(email, user.Email);
+            Assert.AreEqual(password, user.Password);
+            Assert.AreEqual(name, user.Name);
+        }
     }
 }
--- Perfin/Perfin.Models/User.cs
+++ Perfin/Perfin.Models/User.cs
@@ -8,5 +8,10 @@ namespace Perfin.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
     }
 }
