9509272691d7ac0b079921ed61cffc371d0b0d41
Max Claus Nunes <maxcnunes@gmail.com>
2013-01-30 01:08:25 -0200

    Creating the Models Tests Project and simple test for User class. The last commit was wrong

---
+++ Perfin/Perfin.Models.Tests/UserTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Perfin.Models.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void Should_Create_A_Instance_Of_User()
+        {
+            var user = new User();
+            Assert.IsNotNull(user);
+            Assert.IsInstanceOfType(user, typeof(User));
+        }
+    }
+}
