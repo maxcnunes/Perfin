fa88c119b245ca7fe74557b597d2584c5860cc57
rafaelvjb <rafaelbjb@hotmail.com>
2013-02-05 01:01:27 -0200

    add categorytest file and alter csproj

---
+++ Perfin/Perfin.Test/Data/Repository/CategoryRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Perfin.Test.Data.Repository
+{
+    [TestClass]
+    public class CategoryRepositoryTest
+    {
+        //IUnityOfWork unitOfWork;
+        //IList<Category> 
+
+
+        [TestMethod]
+        public void Should_Add_10_Categories()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
