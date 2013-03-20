31584cc5f6c8b909117c099ec5c8e8401e5b54fb
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 10:14:17 -0300

    Move routes configurations into the new config file

--- Perfin/Perfin.UI.Web/App/viewmodels/shell.js
+++ Perfin/Perfin.UI.Web/App/viewmodels/shell.js
@@ -1,14 +1,36 @@
-﻿define(['durandal/plugins/router', 'durandal/app'], function (router, app) {
+﻿define([
+    'durandal/plugins/router',
+    'durandal/app',
+    'config'],
+    function (router, app, config) {
 
-    return {
-        router: router,
-        search: function() {
+    var
+        router = router,
+        search = function () {
             //It's really easy to show a message box.
             //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         },
-        activate: function () {
-            return router.activate('welcome');
-        }
+        activate = function () {
+            //return datacontext.primeData()
+            //    .then(boot)
+            //    .fail(failedInitialization);
+
+            return boot();
+        },
+        boot = function() {
+            //logger.log('App Loaded!', null, system.getModuleId(shell), true);
+            router.map(config.routes);
+            return router.activate(config.startModule);
+        },
+        failedInitialization = function (error) {
+            var msg = 'App initialization failed: ' + error.message;
+            logger.logError(msg, error, system.getModuleId(shell), true);
+        };
+
+    return {
+        router: router,
+        search: search,
+        activate: activate
     };
 });
