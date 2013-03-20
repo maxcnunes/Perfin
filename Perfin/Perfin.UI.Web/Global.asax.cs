b9092ee8970e6b0970683c9ba4b2b6959ffd945a
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 19:47:33 -0300

    Configure Web Api to reponse on JSON format

--- Perfin/Perfin.UI.Web/Global.asax.cs
+++ Perfin/Perfin.UI.Web/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -25,7 +21,7 @@ namespace Perfin.UI.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            //GlobalConfig.CustomizeConfig(GlobalConfiguration.Configuration);
+            GlobalConfig.CustomizeConfig(GlobalConfiguration.Configuration);
         }
     }
 }
