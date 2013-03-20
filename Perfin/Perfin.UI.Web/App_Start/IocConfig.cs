d8e8d7ac172309f361b8ac5872cbb95c0f99dff1
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 19:27:04 -0300

    Add and Configure IOC Ninject lib

---
+++ Perfin/Perfin.UI.Web/App_Start/IocConfig.cs
@@ -0,0 +1,31 @@
+﻿using Ninject;
+using Perfin.Data;
+using Perfin.Data.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Perfin.UI.Web
+{
+    public class IocConfig
+    {
+        public static void RegisterIoc(HttpConfiguration config)
+        {
+            var kernel = new StandardKernel(); // Ninject Ioc
+
+            // These registrations are "per instance request"
+            // See http://blog.bobcravens.com/2010/03/ninject-life-cycle-management-or-scoping/
+
+            kernel.Bind<RepositoryFactories>().To<RepositoryFactories>()
+                .InSingletonScope();
+
+            kernel.Bind<IRepositoryProvider>().To<RepositoryProvider>();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+
+            // Tell WebApi how to use our Ninject Ioc
+            config.DependencyResolver = new NinjectDependencyResolver(kernel);
+        }
+    }
+}
