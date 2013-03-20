d413f3ca767785ad75010fda5618f9dedb29aa6d
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 19:51:44 -0300

    Move DurandalBundleConfig to BundleConfig class.. And move DurandalConfig to Global.ascx

--- Perfin/Perfin.UI.Web/App_Start/BundleConfig.cs
+++ Perfin/Perfin.UI.Web/App_Start/BundleConfig.cs
@@ -1,106 +1,50 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Optimization;
 
 namespace Perfin.UI.Web
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // Force optimization to be on or off, regardless of web.config setting
-            //BundleTable.EnableOptimizations = false;
-            bundles.UseCdn = false;
-
-            // .debug.js, -vsdoc.js and .intellisense.js files 
-            // are in BundleTable.Bundles.IgnoreList by default.
-            // Clear out the list and add back the ones we want to ignore.
-            // Don't add back .debug.js.
             bundles.IgnoreList.Clear();
-            bundles.IgnoreList.Ignore("*-vsdoc.js");
-            bundles.IgnoreList.Ignore("*intellisense.js");
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
 
-            // Modernizr goes separate since it loads first
-            bundles.Add(new ScriptBundle("~/bundles/modernizr")
-                .Include("~/Scripts/lib/modernizr-{version}.js"));
+            bundles.Add(
+              new ScriptBundle("~/scripts/vendor")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/knockout-{version}.js")
+                .Include("~/Scripts/sammy-{version}.js")
+                .Include("~/Scripts/bootstrap.min.js")
+                .Include("~/Scripts/toastr.min.js")
+                .Include("~/Scripts/amplify.js")
+              );
 
-            // jQuery
-            bundles.Add(new ScriptBundle(
-                "~/bundles/jquery",
-                "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
-            ).Include("~/Scripts/lib/jquery-{version}.js"));
+            bundles.Add(
+              new StyleBundle("~/Content/css")
+                .Include("~/Content/ie10mobile.css")
+                .Include("~/Content/bootstrap.min.css")
+                .Include("~/Content/bootstrap-responsive.min.css")
+                .Include("~/Content/font-awesome.min.css")
+                .Include("~/Content/toastr.css")
+                .Include("~/Content/durandal.css")
+                .Include("~/Content/app.css")
+              );
+        }
 
-            // 3rd Party JavaScript files
-            bundles.Add(new ScriptBundle("~/bundles/jsextlibs").Include(
-                "~/Scripts/lib/json2.js", // IE7 needs this
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            if (ignoreList == null)
+            {
+                throw new ArgumentNullException("ignoreList");
+            }
 
-                // NOT USED YET
-                //// jQuery plugins
-                //"~/Scripts/lib/activity-indicator.js",
-                //"~/Scripts/lib/jquery.mockjson.js",
-                "~/Scripts/lib/TrafficCop.js",
-                "~/Scripts/lib/infuser.js", // depends on TrafficCop
-
-                // Knockout and its plugins
-                "~/Scripts/lib/knockout-{version}.js",
-                "~/Scripts/lib/knockout.activity.js",
-                "~/Scripts/lib/knockout.asyncCommand.js",
-                "~/Scripts/lib/knockout.dirtyFlag.js",
-                "~/Scripts/lib/knockout.validation.js",
-                "~/Scripts/lib/koExternalTemplateEngine.js",
-
-
-                // Other 3rd party libraries
-                "~/Scripts/lib/underscore.js",
-                //"~/Scripts/lib/moment.js",
-                "~/Scripts/lib/sammy-{version}.js",
-                "~/Scripts/lib/amplify.*",
-                "~/Scripts/lib/toastr-{version}.js"
-            ));
-
-            // NOT USED YET
-            //bundles.Add(new ScriptBundle("~/bundles/jsmocks")
-            //    .IncludeDirectory("~/Scripts/app/mock", "*.js", searchSubdirectories: false));
-
-            // All application JS files (except mocks)
-            bundles.Add(new ScriptBundle("~/bundles/jsapplibs")
-                .IncludeDirectory("~/Scripts/app/", "*.js", searchSubdirectories: false));
-
-            // 3rd Party CSS files
-            bundles.Add(new StyleBundle("~/Content/3rdpartycss").Include(
-                // Normalize CSS
-                "~/Content/boilerplate-normalize.css",
-
-                // Twitter Bootstrap
-                "~/Content/bootstrap/css/bootstrap.css",
-                "~/Content/bootstrap/css/bootstrap-responsive.css",
-
-                // Toastr Notifications
-                "~/Content/toastr.css",
-                "~/Content/toastr-responsive.css",
-
-               // Main css
-                "~/Content/main.css"
-            ));
-
-            // Fonts CSS files
-            bundles.Add(new StyleBundle("~/Content/fontscss").Include(
-                // Font Awesome Stylesheet
-                "~/Content/font-awesome/css/font-awesome.css"
-           ));
-
-
-            // App CSS files
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                // Main css
-                "~/Content/main.css"
-            ));
-
-
-            // NOT USED YET
-            //// Custom LESS files
-            //bundles.Add(new Bundle("~/Content/Less", new LessTransform(), new CssMinify())
-            //    .Include("~/Content/styles.less"));
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
         }
     }
 }
