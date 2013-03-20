95cc750b1ed09108c624a102a65747dff0ae7af8
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 10:46:51 -0300

    Config logger messages with toastr

--- Perfin/Perfin.UI.Web/App/viewmodels/shell.js
+++ Perfin/Perfin.UI.Web/App/viewmodels/shell.js
@@ -1,36 +1,38 @@
 ﻿define([
     'durandal/plugins/router',
     'durandal/app',
-    'config'],
-    function (router, app, config) {
+    'config',
+    'services/logger',
+    'durandal/system'],
+    function (router, app, config, logger, system) {
+        var
+            shell = this,
+            router = router,
+            search = function () {
+                //It's really easy to show a message box.
+                //You can add custom options too. Also, it returns a promise for the user's response.
+                app.showMessage('Search not yet implemented...');
+            },
+            activate = function () {
+                //return datacontext.primeData()
+                //    .then(boot)
+                //    .fail(failedInitialization);
 
-    var
-        router = router,
-        search = function () {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
-        },
-        activate = function () {
-            //return datacontext.primeData()
-            //    .then(boot)
-            //    .fail(failedInitialization);
+                return boot();
+            },
+            boot = function () {
+                logger.log('App Loaded!', null, system.getModuleId(shell), true);
+                router.map(config.routes);
+                return router.activate(config.startModule);
+            },
+            failedInitialization = function (error) {
+                var msg = 'App initialization failed: ' + error.message;
+                logger.logError(msg, error, system.getModuleId(shell), true);
+            };
 
-            return boot();
-        },
-        boot = function() {
-            //logger.log('App Loaded!', null, system.getModuleId(shell), true);
-            router.map(config.routes);
-            return router.activate(config.startModule);
-        },
-        failedInitialization = function (error) {
-            var msg = 'App initialization failed: ' + error.message;
-            logger.logError(msg, error, system.getModuleId(shell), true);
+        return {
+            router: router,
+            search: search,
+            activate: activate
         };
-
-    return {
-        router: router,
-        search: search,
-        activate: activate
-    };
-});
+    });
