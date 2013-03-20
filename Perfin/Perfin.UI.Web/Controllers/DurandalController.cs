28a81299882b370e2033bb64f9b7fc2afde55d36
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 16:21:11 -0300

    Remove webpages:enabled option (Now is not necessary use a controller to render a .csthml page)

--- Perfin/Perfin.UI.Web/Controllers/DurandalController.cs
+++
@@ -1,9 +0,0 @@
-using System.Web.Mvc;
-
-namespace Perfin.UI.Web.Controllers {
-  public class DurandalController : Controller {
-    public ActionResult Index() {
-      return View();
-    }
-  }
-}
