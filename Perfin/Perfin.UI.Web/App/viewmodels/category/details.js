bcd501a808801c613c861d99fdcc4a590c0a1753
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-20 00:33:48 -0300

    Simple example of CRU (non-db) for Category module

---
+++ Perfin/Perfin.UI.Web/App/viewmodels/category/details.js
@@ -0,0 +1,22 @@
+﻿define([
+    'durandal/plugins/router',
+    'repositories/category'],
+    function (router, repository) {
+
+    return {
+        categoryToShow: {
+            name: ko.observable(),
+            parent: ko.observable()
+        },
+
+        activate: function (context) {
+            // Grab item from repository
+            var category = repository.getCategory(context.id);
+
+            // Add to view model
+            this.categoryToShow.name(category.name);
+            this.categoryToShow.parent(category.parent);
+        }
+    };
+
+});
