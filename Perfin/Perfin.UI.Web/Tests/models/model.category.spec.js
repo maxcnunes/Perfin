efa34626aaab806c4fe59154c3e6b52f9469e3f1
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-22 01:42:19 -0300

    Create test to isDirty property of model.category module

---
+++ Perfin/Perfin.UI.Web/Tests/models/model.category.spec.js
@@ -0,0 +1,22 @@
+﻿define([
+    'models/model.category'],
+    function (model) {
+        describe('Model :: Category', function () {
+
+            var category = new model();
+
+            it('should be defined as not dirty when initialized', function () {
+
+                expect(category.dirtyFlag().isDirty()).toBe(false);
+            });
+
+            it('should be defined as dirty when any property changed', function () {
+
+                category.id(99);
+                category.name('name test');
+                category.parent(99);
+
+                expect(category.dirtyFlag().isDirty()).toBe(true);
+            });
+        });
+    });
