21896e5a0c965a30d477b4743ad8a75735c1dcba
Max Claus Nunes <maxcnunes@gmail.com>
2013-03-23 17:57:15 -0300

    Configure app layout to be equal was before change the project to use durandal framework

--- Perfin/Perfin.UI.Web/App/viewmodels/category/add.js
+++ Perfin/Perfin.UI.Web/App/viewmodels/category/add.js
@@ -6,89 +6,90 @@
     function (app, datacontext, router, model) {
 
 
-        var isSaving = ko.observable(false),
-        parentCategories = ko.observableArray(),
-        category = ko.observable(),
+        var
+            isSaving = ko.observable(false),
+            parentCategories = ko.observableArray(),
+            category = ko.observable(),
 
-        activate = function () {
-            initLookups();
-            category(new model());
-            //category(datacontext.createSession());
-        },
-        initLookups = function () {
-            //parentCategories(datacontext.lookups.rooms);
-        },
-        cancel = function (complete) {
-            router.navigateBack();
-        },
-        canEditCategory = ko.computed(function () {
-            return category();// && config.currentUser() && config.currentUser().id() === session().speakerId();
-        }),
-        hasChanges = ko.computed(function () {
-            if (canEditCategory()) {
-                return category().dirtyFlag().isDirty();
-            }
+            activate = function () {
+                initLookups();
+                category(new model());
+                //category(datacontext.createSession());
+            },
+            initLookups = function () {
+                //parentCategories(datacontext.lookups.rooms);
+            },
+            cancel = function (complete) {
+                router.navigateBack();
+            },
+            canEditCategory = ko.computed(function () {
+                return category();// && config.currentUser() && config.currentUser().id() === session().speakerId();
+            }),
+            hasChanges = ko.computed(function () {
+                if (canEditCategory()) {
+                    return category().dirtyFlag().isDirty();
+                }
 
-            return false;
-            //return datacontext.hasChanges();
-        }),
-        canSave = ko.computed(function () {
-            return hasChanges() && !isSaving();
-        }),
-        save = function () {
+                return false;
+                //return datacontext.hasChanges();
+            }),
+            canSave = ko.computed(function () {
+                return hasChanges() && !isSaving();
+            }),
+            save = function () {
 
-            // OLD
-            //----------------------------
-            //// Add item to db
-            //repository.addCategory(ko.toJS(this.categoryToAdd));
+                // OLD
+                //----------------------------
+                //// Add item to db
+                //repository.addCategory(ko.toJS(this.categoryToAdd));
 
-            //// flag new item
-            //this._categoryAdded = true;
+                //// flag new item
+                //this._categoryAdded = true;
 
-            //// return to list of items
-            //router.navigateTo("#/category/show");
-
-
-            // NEW EXAMPLE
-            //----------------------------
-            //isSaving(true);
-            //datacontext.saveChanges()
-            //    .then(goToEditView).fin(complete);
-
-            //function goToEditView(result) {
-            //    router.replaceLocation('#/sessiondetail/' + session().id());
-            //}
-
-            //function complete() {
-            //    isSaving(false);
-            //}
-        },
-        canDeactivate = function () {
-            return true;
-
-            // OLD
-            //----------------------------
-            //if (this._categoryAdded == false) {
-            //    return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
-            //} else {
-            //    return true;
-            //}
+                //// return to list of items
+                //router.navigateTo("#/category/show");
 
 
-            // NEW EXAMPLE
-            //----------------------------
-            //if (hasChanges()) {
-            //    var msg = 'Do you want to leave and cancel?';
-            //    return app.showMessage(msg, 'Navigate Away', ['Yes', 'No'])
-            //        .then(function (selectedOption) {
-            //            if (selectedOption === 'Yes') {
-            //                datacontext.cancelChanges();
-            //            }
-            //            return selectedOption;
-            //        });
-            //}
-            //return true;
-        };
+                // NEW EXAMPLE
+                //----------------------------
+                //isSaving(true);
+                //datacontext.saveChanges()
+                //    .then(goToEditView).fin(complete);
+
+                //function goToEditView(result) {
+                //    router.replaceLocation('#/sessiondetail/' + session().id());
+                //}
+
+                //function complete() {
+                //    isSaving(false);
+                //}
+            },
+            canDeactivate = function () {
+                return true;
+
+                // OLD
+                //----------------------------
+                //if (this._categoryAdded == false) {
+                //    return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+                //} else {
+                //    return true;
+                //}
+
+
+                // NEW EXAMPLE
+                //----------------------------
+                //if (hasChanges()) {
+                //    var msg = 'Do you want to leave and cancel?';
+                //    return app.showMessage(msg, 'Navigate Away', ['Yes', 'No'])
+                //        .then(function (selectedOption) {
+                //            if (selectedOption === 'Yes') {
+                //                datacontext.cancelChanges();
+                //            }
+                //            return selectedOption;
+                //        });
+                //}
+                //return true;
+            };
 
         var vm = {
             activate: activate,
@@ -99,7 +100,10 @@
             parentCategories: parentCategories,
             save: save,
             category: category,
-            title: 'Add a New Category'
+
+            // module page info
+            pageDisplayName: 'Create Category',
+            pageDescription: 'Create a category and let more organized your finances'
         };
 
         return vm;
--- Perfin/Perfin.UI.Web/App/viewmodels/welcome.js
+++ Perfin/Perfin.UI.Web/App/viewmodels/welcome.js
@@ -1,20 +1,7 @@
 ﻿define(function() {
     var welcome = function () {
-        this.displayName = 'Welcome to the Durandal Starter Kit!';
-        this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
-        this.features = [
-            'Clean MV* Architecture',
-            'JS & HTML Modularity',
-            'Simple App Lifecycle',
-            'Eventing, Modals, Message Boxes, etc.',
-            'Navigation & Screen State Management',
-            'Consistent Async Programming w/ Promises',
-            'App Bundling and Optimization',
-            'Use any Backend Technology',
-            'Built on top of jQuery, Knockout & RequireJS',
-            'Integrates with other libraries such as SammyJS & Bootstrap',
-            'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
-        ];
+        this.pageDisplayName = 'Dashboard';
+        this.pageDescription = 'All informations about your personal finance on the same place';
     };
 
     welcome.prototype.viewAttached = function (view) {
